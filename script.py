4e8925fc24250d3c8e0977da8c46f1cec402e3cb
Konstantin Shuvalov <shuvalov.2021@list.ru>
2022-05-23 07:22:23 +0300

    feat: add record result

--- script.py
+++ script.py
@@ -14,3 +14,9 @@ for root, dirs, files in os.walk(currentDir):
 
 #sort keys
 sortedFileList = sorted(filelist.keys())
+
+result = open("result.txt", "w+")
+
+for value in sortedFileList:
+	file = open(filelist[value], encoding="utf8").read()
+	result.write(file)
