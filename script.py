4921b1e4e11e0ed359c3c540200e3766c3d69815
Konstantin Shuvalov <shuvalov.2021@list.ru>
2022-05-23 07:19:28 +0300

    feat: add loop to find txt file

--- script.py
+++ script.py
@@ -5,3 +5,10 @@ files = os.listdir(currentDir)
 
 #txt file collection
 filelist = {}
+
+#find txt file 
+for root, dirs, files in os.walk(currentDir):
+	for file in files: 
+		if(file.endswith( ".txt" ) and file != "result.txt"):
+			filelist[file] = os.path.join(root,file)
+
